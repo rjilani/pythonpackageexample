04d63d34a73d6203a5020851a92e4cc29ab4989a
Rashid Jilani <rjilani@skytouchtechnology.com>
2020-02-21 15:17:01 -0600

    python package how to example

---
+++ driver.py
@@ -0,0 +1,4 @@
+from package1.util import add
+from package2.util import mul
+
+print(mul(2,5))
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='rashidtest',
+	version="1.0.0",
+    packages=['package1','package2'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='test',
+)
